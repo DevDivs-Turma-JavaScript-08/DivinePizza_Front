49a24a5d6e67750b8c5967cce8b51d93d46733c1
Grazielle Gualter <grazi.gualter@gmail.com>
2025-09-10 15:12:29 -0300

    Atualização página

--- src/pages/insights/Insights.tsx
+++ src/pages/insights/Insights.tsx
@@ -1,34 +1,49 @@
 function Insights() {
   return (
     <div>
-      <div className="bg-white">
-        <div className="bg-amber-200 flex justify-center gap-8 p-4 ">
-          <div className="bg-(--secondary) rounded-2xl text-center py-3.5 px-5 text-white">
+      <div className="flex flex-col rounded-2xl bg-[#E77F7350] m-10" >
+        <div className=" flex justify-center gap-8 p-4  ">
+          <div className="bg-(--secondary) rounded-2xl text-center py-3.5 px-8 text-white">
             <p className="text-4xl"> 100</p>
-            <p className="text-sm"> Quantidade de pedidos por mês</p>
+            <p className="text-sm"> Quantidade de pedidos <br/> por mês</p>
           </div>
-          <div>
-            <p>R$</p> <p>Lucro liquido por mês</p>
+          <div className ="bg-(--secondary) rounded-2xl text-center py-3.5 px-8 text-white ">
+            <p className="text-4xl"> </p>
+            <p>R$</p> <p>Lucro Liquido por Mês</p>
           </div>
-          <div>
-            <p> estrela </p> <p>Avaliacao Geral</p>
+          <div className ="bg-(--secondary) rounded-2xl text-center py-3.5 px-13 text-white">
+            <p className="text-4xl"></p>
+            <p> ★ </p> <p>Avaliaçao Geral</p>
           </div>
-          <div>
-            <p> % </p> <p>Indice de fidelidade do cliente</p>
+          <div className ="bg-(--secondary) rounded-2xl text-center py-3.5 px-9 text-white">
+            <p> % </p> <p>Índice de Fidelidade <br/> do Cliente</p>
           </div>
-          <div>
+          <div className ="bg-(--secondary) rounded-2xl text-center py-3.5 px-6 text-white">
+            <p className="text-4xl"> </p>
             <p> R$ </p>
-            <p>Gasto médio com fornecedores por mês</p>
+            <p>Gasto Médio com <br/>Fornecedores por Mês</p>
           </div>
         </div>
-        <div className="bg-amber-300 flex justify-center gap-8">
-          <div>grafico</div>
-          <div>sabor mais pedido</div>
-          <div>sabor menos pedido</div>
+        <div className=" flex justify-center gap-8 p-4">
+          <div className ="bg-(--secondary) rounded-2xl text-center py-25.5 px-64 text-white">Grafico</div>
+          <div className ="bg-(--secondary) rounded-2xl text-center py-25.5 px-16 text-white">
+            <div className="bg-white rounded-full w-35 h-35 "></div>
+            Sabor mais pedido</div>
+          <div className ="bg-(--secondary) rounded-2xl text-center py-25.5 px-16 text-white">
+            <div className="bg-white rounded-full w-35 h-35 "></div>
+            Sabor menos pedido</div>
         </div>
-        <div className="bg-amber-400 flex justify-center gap-8">
-          <div>ultimos pedidos</div>
-          <div>outros itens</div>
+        <div className=" flex justify-center gap-8 p-4">
+          <div className ="bg-(--secondary) rounded-2xl text-center py-25.5 px-56 text-white">
+            Ultimos pedidos 
+            
+            </div>
+          
+          
+          <div className ="bg-(--secondary) rounded-2xl text-center py-25.5 px-60 text-white">
+            Outros itens
+            
+            </div>
         </div>
       </div>
     </div>
