95e5a647d7746562953400e98ea93ae9f0ae4d9f
Pedro Henrique <karpatech2025@gmail.com>
2025-09-10 12:03:49 -0300

    TudoNosso

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import Footer from './components/footer/Footer'
+import InfoCLientes from './components/infoclientes/InfoClientes'
 import Navbar from './components/navbar/Navbar'
 import Clientes from './pages/clientes/Clientes'
-
+import ClientCard from './components/clientcard/ClientCard'
 function App() {
 
   return (
@@ -9,6 +10,8 @@ function App() {
       <Navbar />
       <Clientes />
       <Footer />
+      <InfoCLientes />
+      <ClientCard />
     </div>
   )
 }
